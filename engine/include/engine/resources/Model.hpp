6baeca7dd57c6482ee28033fc33477c5ee7a2904
Zeljko <zeljkonikolic1611@gmail.com>
2026-09-14 15:47:51 +0200

    Added function for drawing lights in a row

--- engine/include/engine/resources/Model.hpp
+++ engine/include/engine/resources/Model.hpp
@@ -10,79 +10,86 @@
 #include <engine/resources/Mesh.hpp>
 #include <utility>
 
-namespace engine::resources {
-/**
-* @class Model
-* @brief Represents a model object within the OpenGL context as an array of @ref Mesh objects.
-*/
-class Model {
-    friend class ResourcesController;
+namespace engine::resources
+{
+    /**
+    * @class Model
+    * @brief Represents a model object within the OpenGL context as an array of @ref Mesh objects.
+    */
+    class Model
+    {
+        friend class ResourcesController;
 
-public:
-    /**
-    * @brief Draws the model using a given shader by drawing all the meshes in the model.
-    * @param shader The shader to use for drawing.
-    */
-    void draw(const Shader *shader);
+    public:
+        /**
+        * @brief Draws the model using a given shader by drawing all the meshes in the model.
+        * @param shader The shader to use for drawing.
+        */
+        void draw(const Shader* shader);
 
-    /**
-    * @brief Destroys the model in the OpenGL context.
-    */
-    void destroy();
+        void draw_range(const Shader* shader, uint32_t first_mesh, uint32_t mesh_count);
+        /**
+        * @brief Destroys the model in the OpenGL context.
+        */
+        void destroy();
 
-    /**
-    * @brief Returns the meshes in the model.
-    * @returns The meshes in the model.
-    */
-    const std::vector<Mesh> &meshes() const {
-        return m_meshes;
-    }
+        /**
+        * @brief Returns the meshes in the model.
+        * @returns The meshes in the model.
+        */
+        const std::vector<Mesh>& meshes() const
+        {
+            return m_meshes;
+        }
 
-    /**
-    * @brief Returns the path to the model file from which the model was loaded.
-    * @returns The path to the model.
-    */
-    const std::filesystem::path &path() const {
-        return m_path;
-    }
+        /**
+        * @brief Returns the path to the model file from which the model was loaded.
+        * @returns The path to the model.
+        */
+        const std::filesystem::path& path() const
+        {
+            return m_path;
+        }
 
-    /**
-    * @brief Returns the name of the model by which it can be referenced using the @ref engine::resources::ResourcesController::model function.
-    * @returns The name of the model.
-    */
-    const std::string &name() const {
-        return m_name;
-    }
+        /**
+        * @brief Returns the name of the model by which it can be referenced using the @ref engine::resources::ResourcesController::model function.
+        * @returns The name of the model.
+        */
+        const std::string& name() const
+        {
+            return m_name;
+        }
 
-private:
-    /**
-    * @brief The meshes in the model.
-    */
-    std::vector<Mesh> m_meshes;
-    /**
-    * @brief The path to the model file from which the model was loaded.
-    */
-    std::filesystem::path m_path;
-    /**
-    * @brief The name of the model by which it can be referenced using the @ref engine::resources::ResourcesController::model function.
-    */
-    std::string m_name;
+    private:
+        /**
+        * @brief The meshes in the model.
+        */
+        std::vector<Mesh> m_meshes;
+        /**
+        * @brief The path to the model file from which the model was loaded.
+        */
+        std::filesystem::path m_path;
+        /**
+        * @brief The name of the model by which it can be referenced using the @ref engine::resources::ResourcesController::model function.
+        */
+        std::string m_name;
 
-    Model() = default;
+        Model() = default;
 
-    /**
-    * @brief Constructs a Model object. Used internally by the @ref engine::resources::ResourcesController class. You are not supposed to call this constructor directly from user code.
-    * @param meshes The meshes in the model.
-    * @param path The path to the model file from which the model was loaded.
-    * @param name The name of the model by which it can be referenced using the @ref engine::resources::ResourcesController::model function.
-    */
-    Model(std::vector<Mesh> meshes, std::filesystem::path path,
-          std::string name)
-        : m_meshes(std::move(meshes))
-        , m_path(std::move(path))
-        , m_name(std::move(name)) {
-    }
-};
-}// namespace engine::resources
+        /**
+        * @brief Constructs a Model object. Used internally by the @ref engine::resources::ResourcesController class. You are not supposed to call this constructor directly from user code.
+        * @param meshes The meshes in the model.
+        * @param path The path to the model file from which the model was loaded.
+        * @param name The name of the model by which it can be referenced using the @ref engine::resources::ResourcesController::model function.
+        */
+        Model(std::vector<Mesh> meshes, std::filesystem::path path,
+              std::string name)
+            : m_meshes(std::move(meshes))
+              , m_path(std::move(path))
+              , m_name(std::move(name))
+        {
+        }
+    };
+} // namespace engine::resources
 
 #endif//MATF_RG_PROJECT_MODEL_HPP
--- engine/src/Model.cpp
+++ engine/src/Model.cpp
@@ -1,19 +1,34 @@
 
 #include <engine/resources/Model.hpp>
 #include <engine/resources/Shader.hpp>
+#include <algorithm>
 
-namespace engine::resources {
-
-void Model::draw(const Shader *shader) {
-    shader->use();
-    for (auto &mesh: m_meshes) {
-        mesh.draw(shader);
+namespace engine::resources
+{
+    void Model::draw(const Shader* shader)
+    {
+        shader->use();
+        for (auto& mesh : m_meshes)
+        {
+            mesh.draw(shader);
+        }
     }
-}
 
-void Model::destroy() {
-    for (auto &mesh: m_meshes) {
-        mesh.destroy();
+    void Model::draw_range(const Shader* shader, uint32_t first_mesh, uint32_t mesh_count)
+    {
+        shader->use();
+        const uint32_t last_mesh = std::min(first_mesh + mesh_count, static_cast<uint32_t>(m_meshes.size()));
+        for (uint32_t index = first_mesh; index < last_mesh; ++index)
+        {
+            m_meshes[index].draw(shader);
+        }
     }
-}
-}// namespace engine::resources
+
+    void Model::destroy()
+    {
+        for (auto& mesh : m_meshes)
+        {
+            mesh.destroy();
+        }
+    }
+} // namespace engine::resources
